25aedbf7b9f8133db2e3bebee78737da9f9d551d
Paulo Neto <pasvsi@gmail.com>
2025-06-25 22:44:58 -0300

    Inicio da modelagem do schema pedido

---
+++ compose/docker/setup.sql
@@ -0,0 +1,7 @@
+# create databases
+CREATE DATABASE IF NOT EXISTS `ecommerce-produtos`;
+CREATE DATABASE IF NOT EXISTS `ecommerce-pedidos`;
+
+# create root user and grant rights
+# CREATE USER 'root'@'localhost' IDENTIFIED BY 'Tr3x2@25';
+# GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';
